7e84a63854972fb2c6024ad41dd518f264a42c3d
Jayant Pahuja <jayant746@gmail.com>
2015-04-19 01:35:43 +0530

    ENH: Completed svm implementation.

--- svmmodel.py
+++ svmmodel.py
@@ -1,5 +1,7 @@
 from data_model import Data
 import pandas as pd
+from sklearn.svm import SVC
+import numpy as np
 
 
 class TitanicData(Data):
@@ -8,12 +10,25 @@ class TitanicData(Data):
     def _preprocess(self):
         """Preprocess the titanic trainset"""
         data = self.data
-        data.drop(['PassengerId', 'Ticket', 'Cabin'], axis=1, inplace=True)
+        data.drop(['PassengerId', 'Ticket', 'Cabin', 'Name'], axis=1, inplace=True)
+        replacer = {'S': 10, 'C': 11, 'Q': 12}
+        data.Embarked = data.Embarked.apply(lambda x: replacer[x] if pd.notnull(x) else np.nan)
+        data.Sex = data.Sex.apply(lambda x: 0 if x == 'male' else 1)
+        data.dropna(inplace=True)
         self.data = data
         
 
 if __name__ == "__main__":
-    d = TitanicData('data/train.csv')
-    processed_data = d.train()
-    print processed_data.head()
+    dataset = TitanicData('data/train.csv')
+    trainset = dataset.train()
+    y = trainset['Survived']
+    X = trainset.drop('Survived', axis=1)
 
+    clf = SVC(kernel='rbf')
+    clf.fit(X, y)
+    validationset = dataset.validation()
+    X_v = validationset.drop('Survived', axis=1)
+    y_v = validationset['Survived']
+    print clf.score(X_v, y_v)
+    
+    
